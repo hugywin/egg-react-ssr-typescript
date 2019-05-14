cdc49c23a3feeb389188c81eef08985d85838f4a
椛船 <jayson.zsj@alibaba-inc.com>
2019-05-14 15:13:15 +0800

    zhusjfaker

---
+++ app/extend/context.ts
@@ -0,0 +1,24 @@
+import { Extend } from '../decorator/extend';
+import { Context } from 'egg';
+import * as path from 'path';
+const { renderToNodeStream } = require('react-dom/server')
+
+@Extend
+class ContextExtend {
+
+    async renderToStream() {
+        const self: Context = this as any;
+        self.type = 'text/html'
+        self.status = 200
+        const isLocal = self.app.config.env === 'local'
+        if (isLocal) {
+            // 本地开发环境下每次刷新的时候清空require服务端文件的缓存，保证服务端与客户端渲染结果一致
+            delete require.cache[path.resolve(__dirname, '../../dist/Page.server.js')]
+        }
+        let serverStream = require('../../dist/Page.server')
+        const serverRes = await serverStream.default(this)
+        const stream = renderToNodeStream(serverRes)
+        self.body = stream
+    }
+}
+export default Reflect.getOwnMetadata("core-enum", ContextExtend.prototype) as ContextExtend;
--- app/router.ts
+++ app/router.ts
@@ -1,7 +1,18 @@
 import { Application } from 'egg';
+import { routelist, RouteInfo } from './decorator/route';
 
 export default (app: Application) => {
   const { controller, router } = app;
 
-  router.get('/', controller.home.index);
+  /** 自动注入路由 */
+  const list = new Array<RouteInfo>().concat(routelist);
+  list.forEach(p => {
+    if (p.origin !== "home") {
+      router.all(p.routeUrl, Reflect.get(Reflect.get(Reflect.get(controller, p.origin), p.controller), p.action));
+    }
+    else {
+      router.all(p.routeUrl, Reflect.get(Reflect.get(controller, p.controller), p.action));
+    }
+  });
+
 };
