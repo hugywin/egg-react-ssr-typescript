79191b7c23f8204106597160118118ec4838cdc0
椛船 <jayson.zsj@alibaba-inc.com>
2019-06-03 08:54:37 +0800

    zhusjfaker

--- config/plugin.ts
+++ config/plugin.ts
@@ -1,11 +1,7 @@
 import { EggPlugin } from 'egg';
 
 const plugin: EggPlugin = {
-  // static: true,
-  // nunjucks: {
-  //   enable: true,
-  //   package: 'egg-view-nunjucks',
-  // },
+  
 };
 
 export default plugin;
